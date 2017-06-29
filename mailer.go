e5f0693820414c6cfde475f11974d7a0645517b9
Antonio Pagano <apagano@wawand.co>
2017-06-28 20:09:48 -0500

    simplifying the Mailer implementation so we build the box only for the SMTP mailer

--- mailer.go
+++ mailer.go
@@ -1,32 +1,39 @@
-package mailer
+package buffalo
 
 import (
 	"strconv"
 
 	"github.com/gobuffalo/envy"
+	"github.com/gobuffalo/packr"
 	gomail "gopkg.in/gomail.v2"
 )
 
+//EmailData is the args passed to render.
+type EmailData map[string]interface{}
+
 //Mailer is an interface for different types of mailers each one will implement needed logics.
 type Mailer interface {
-	Send(m Message) error
+	Send(m EmailData) error
 }
 
-type smtpMailer struct {
-	dialer *gomail.Dialer
+//SMTPMailer is the first implementation of the mailer interface.
+type SMTPMailer struct {
+	Dialer      *gomail.Dialer
+	TemplateBox packr.Box
 }
 
 //Send a message using SMTP configuration or returns an error if something goes wrong.
-func (sm smtpMailer) Send(m Message) error {
+func (sm SMTPMailer) Send(data EmailData) error {
 	return nil
 }
 
 //NewSMTPMailer Creates an SMTP mailer by reading configuration from the env or using defaults.
-func NewSMTPMailer() smtpMailer {
+func NewSMTPMailer(box packr.Box) *SMTPMailer {
 	port, _ := strconv.Atoi(envy.Get("SMTP_PORT", "1025"))
 	dialer := gomail.NewDialer(envy.Get("SMTP_HOST", "localhost"), port, envy.Get("SMTP_USER", ""), envy.Get("SMTP_PASSWORD", ""))
 
-	return smtpMailer{
-		dialer: dialer,
+	return &SMTPMailer{
+		Dialer:      dialer,
+		TemplateBox: box,
 	}
 }
