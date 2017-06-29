f7a7922140c67128a3ab3d688c8b3cdf5c369968
Antonio Pagano <apagano@wawand.co>
2017-06-28 20:48:52 -0500

    organizing mailer a bit

--- mailer.go
+++ mailer.go
@@ -5,15 +5,38 @@ import (
 
 	"github.com/gobuffalo/envy"
 	"github.com/gobuffalo/packr"
+	"github.com/gobuffalo/plush"
+	"github.com/pkg/errors"
 	gomail "gopkg.in/gomail.v2"
 )
 
-//EmailData is the args passed to render.
-type EmailData map[string]interface{}
+//EmailData is the data needed to send the email.
+type EmailData struct {
+	From    string
+	To      string
+	Subject string
+	//Other fields that we should add here.
+	TemplateData map[string]interface{}
+}
+
+//BuildBody builds the message body based on pased data and the template.
+func (m EmailData) BuildBody(templatesBox packr.Box, templatePath string, data EmailData) (string, error) {
+	tmpl, err := templatesBox.MustString(templatePath)
+	if err != nil {
+		return "", err
+	}
+
+	s, err := plush.Render(tmpl, plush.NewContextWith(data.TemplateData))
+	if err != nil {
+		return "", err
+	}
+
+	return s, nil
+}
 
 //Mailer is an interface for different types of mailers each one will implement needed logics.
 type Mailer interface {
-	Send(m EmailData) error
+	Send(m EmailData, templatePath string, contentType string) error
 }
 
 //SMTPMailer is the first implementation of the mailer interface.
@@ -23,16 +46,34 @@ type SMTPMailer struct {
 }
 
 //Send a message using SMTP configuration or returns an error if something goes wrong.
-func (sm SMTPMailer) Send(data EmailData) error {
+func (sm SMTPMailer) Send(data EmailData, templatePath string, contentType string) error {
+	message := gomail.NewMessage()
+
+	message.SetHeader("From", data.From)
+	message.SetHeader("To", data.To)
+	message.SetHeader("Subject", data.Subject)
+
+	body, err := data.BuildBody(sm.TemplateBox, templatePath, data)
+	if err != nil {
+		return errors.WithStack(err)
+	}
+	message.SetBody(contentType, body)
+
+	err = sm.Dialer.DialAndSend(message)
+
+	if err != nil {
+		return errors.WithStack(err)
+	}
+
 	return nil
 }
 
 //NewSMTPMailer Creates an SMTP mailer by reading configuration from the env or using defaults.
-func NewSMTPMailer(box packr.Box) *SMTPMailer {
+func NewSMTPMailer(box packr.Box) SMTPMailer {
 	port, _ := strconv.Atoi(envy.Get("SMTP_PORT", "1025"))
 	dialer := gomail.NewDialer(envy.Get("SMTP_HOST", "localhost"), port, envy.Get("SMTP_USER", ""), envy.Get("SMTP_PASSWORD", ""))
 
-	return &SMTPMailer{
+	return SMTPMailer{
 		Dialer:      dialer,
 		TemplateBox: box,
 	}
